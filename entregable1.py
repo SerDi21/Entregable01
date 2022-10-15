9c854b8a06c5adbbe36b7b9cb622e589b929ead6
Sergio Diaz <100312184+SerDi21@users.noreply.github.com>
2022-10-15 13:21:59 +0200

    Update entregable1.py

--- entregable1.py
+++ entregable1.py
@@ -45,36 +45,55 @@ def read_data(f: TextIO) -> tuple[UndirectedGraph[Vertex], int, int]:
 
 
 def process(lab: UndirectedGraph[Vertex], rows: int, cols: int) -> tuple[Optional[Edge], int, int]:
-    e=bf_search(lab,(0,0),(rows-1,cols-1))
-    s=bf_search(lab,(rows-1,cols-1),(0,0))
+    final: Vertex= rows-1,cols-1
+    e=bf_search(lab,(0,0),final)
+    s=bf_search(lab,final,(0,0))
+    len_bf:int= e.get(final)
+    fn_len=len_bf
+    edge = ((0, 0), (0, 0))
+    for r in range(rows):
+        for c in range(cols):
+            if e.get((r, c)) is not None and s.get((r+1, c)) is not None:
+                suma1 = e.get((r, c)) + s.get((r + 1, c))
+                if suma1<fn_len:
+                    fn_len = suma1
+                    edge = ((r,c),(r+1,c))
+            if e.get((r, c)) is not None and s.get((r, c+1)) is not None:
+                suma2 = e.get((r, c)) + s.get((r, c + 1))
+                if suma2<fn_len:
+                    fn_len = suma2
+                    edge = ((r,c),(r,c+1))
+    print(fn_len)
+    if edge == ((0, 0), (0, 0)):
+        return None, len_bf, len_bf
+    return edge, len_bf, fn_len
 
-    return
 
-
-def bf_search(graph: UndirectedGraph[Vertex],vIni: Vertex, vFin: Vertex) -> list[tuple[Vertex,int]]:
-    edges: list[Edge]=[]
+def bf_search(graph: UndirectedGraph[Vertex],vIni: Vertex, vFin: Vertex) -> dict[Vertex,int]:
+    mapa: dict[Vertex,int] = dict()
+    mapa[vIni] = 0
     seen: set[Vertex]=set()
     seen.add(vIni)
     queue: Fifo[Edge]=Fifo()
     queue.push((vIni,vIni))
     while len(queue)>0:
         u,v= queue.pop()
-        edges.append((u,v))
         if v==vFin:
-            return edges
+            return mapa
         for suc in graph.succs(v):
             if suc not in seen:
+                i = mapa.get(v)+1
+                mapa[suc] = i
                 queue.push((v, suc))
                 seen.add(suc)
-
-    return edges
+    return mapa
 
 
 def show_results(edge_to_add: Optional[Edge], length_before: int, length_after: int):
     if edge_to_add is None:
         print(NO_VALID_WALL)
     else:
-
+        print(edge_to_add)
     print(length_before)
     print(length_after)
 
